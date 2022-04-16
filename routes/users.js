761f6f4d19d3a3b594a66515b3a3e39a3f3586b2
Rahib uzair ahmed <52162363+samxoxoxo@users.noreply.github.com>
2022-04-16 06:02:44 +0530

    hi

--- routes/users.js
+++ routes/users.js
@@ -3,7 +3,7 @@ var router = express.Router();
 const fs = require('fs')
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-  fs.readFile('E:/BDAT/Data Programming/workload/grpAss/front-end/response.json', (err, response) =>{
+  fs.readFile('./routes/response.json', (err, response) =>{
     if(err) {
       console.log(err)
     } else {
